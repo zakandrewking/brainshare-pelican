abacbaf4ab7ac148676a0ab432ebf224b5fde3f6
Zachary King <zak@delfina.com>
2023-05-04 13:15:33 -0700

    🚅

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <button className="btn btn-primary">Button</button>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
