58ac02558f4f9f0003a70eb1c544fa48d58d827f
Zachary King <zak@delfina.com>
2023-05-04 09:50:07 -0700

    🗼

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  basePath: "/pelican",
+  // for debugging, redirect localhost to /metabolism
+  ...(process.env.NODE_ENV === "development" && {
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/pelican",
+          basePath: false,
+          permanent: false,
+        },
+      ];
+    },
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
